694211b79c8d4bd98b9f20536f892a7d4039db8d
cosmic-world <manasroy974@gmail.com>
2025-08-16 18:26:33 +0530

    list

--- src/node.js
+++ src/node.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 // Specify the folder containing the files
-const folderPath = 'E:/TNSO-JRM/Patna'; // Replace with your folder path
+const folderPath = 'E:/TNSO-JRM/sankari2'; // Replace with your folder path
 
 // Function to rename files to lowercase and save their names
 const renameFilesToLowercase = (folderPath) => {
